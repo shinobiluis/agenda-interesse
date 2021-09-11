22555ff6d2e57583b4f0f8ec618d1d5795b2d2b7
Luis Eduardo Alcantara Olvera <shinobi_luis@hotmail.es>
2021-09-10 22:03:53 -0500

    Se pasan todas las modales a una carpeta

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -17,10 +17,10 @@
             </div>
         </nav>
         @include('tablaRegistros')
-        @include('agregar')
-        @include('modalVerTelefonos')
-        @include('modalAgregarTelefono')
-        @include('modalEditarUsuario')
+        @include('modals.agregar')
+        @include('modals.modalVerTelefonos')
+        @include('modals.modalAgregarTelefono')
+        @include('modals.modalEditarUsuario')
     </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
